6492843a76037101dd3f9a02ba14d1dfb371e6fb
Yifan Hou <yifanhou@stanford.edu>
2024-07-07 17:28:33 -0700

    Added yaml config deserialization to admittance controller
    * Adding dependency on yaml-cpp
    
    Test: tested on UR5e

---
+++ src/config_deserialize.cpp
@@ -0,0 +1,44 @@
+#include "force_control/config_deserialize.h"
+#include <RobotUtilities/utilities.h>
+
+#include <yaml-cpp/yaml.h>
+
+#include "force_control/admittance_controller.h"
+
+template <>
+bool deserialize(const YAML::Node& node,
+                 AdmittanceController::AdmittanceControllerConfig& config) {
+  try {
+    config.dt = node["dt"].as<double>();
+    config.log_to_file = node["log_to_file"].as<bool>();
+    config.log_file_path = node["log_file_path"].as<std::string>();
+    config.compliance6d.stiffness = RUT::deserialize_vector<RUT::Vector6d>(
+                                        node["compliance6d"]["stiffness"])
+                                        .asDiagonal();
+    config.compliance6d.damping =
+        RUT::deserialize_vector<RUT::Vector6d>(node["compliance6d"]["damping"])
+            .asDiagonal();
+    config.compliance6d.inertia =
+        RUT::deserialize_vector<RUT::Vector6d>(node["compliance6d"]["inertia"])
+            .asDiagonal();
+    config.direct_force_control_gains.P_trans =
+        node["direct_force_control_gains"]["P_trans"].as<double>();
+    config.direct_force_control_gains.I_trans =
+        node["direct_force_control_gains"]["I_trans"].as<double>();
+    config.direct_force_control_gains.D_trans =
+        node["direct_force_control_gains"]["D_trans"].as<double>();
+    config.direct_force_control_gains.P_rot =
+        node["direct_force_control_gains"]["P_rot"].as<double>();
+    config.direct_force_control_gains.I_rot =
+        node["direct_force_control_gains"]["I_rot"].as<double>();
+    config.direct_force_control_gains.D_rot =
+        node["direct_force_control_gains"]["D_rot"].as<double>();
+    config.direct_force_control_I_limit =
+        RUT::deserialize_vector<RUT::Vector6d>(
+            node["direct_force_control_I_limit"]);
+  } catch (const std::exception& e) {
+    std::cerr << "Failed to load the config file: " << e.what() << std::endl;
+    return false;
+  }
+  return true;
+}
